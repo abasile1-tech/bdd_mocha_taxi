0a1de6173598c7cf4cedefba61b9772f4bea20ff
John <ashton.basile@gmail.com>
2023-08-08 14:34:35 +0100

    next, I need to add a model property

--- specs/taxi_spec.js
+++ specs/taxi_spec.js
@@ -1,2 +1,16 @@
 const assert = require('assert');
 const Taxi = require('../taxi.js');
+
+describe('Taxi', function (){
+	it('should have a manufacturer', function () {
+		const taxi = new Taxi('Toyota', 'Prius'); // Arrange
+		const actual = taxi.manufacturer; // Act
+		assert.strictEqual(actual, 'Toyota'); // Assert
+	});
+
+	it('should have a model', function () {
+		const taxi = new Taxi('Toyota', 'Prius');
+		const actual = taxi.model;
+		assert.strictEqual(actual, 'Prius');
+	});
+});
